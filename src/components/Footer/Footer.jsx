f8281a362ec1c1dff9af4eb21831130427e7fa62
JuliaMichaela <juliaperez@yandex.ru>
2023-11-23 12:44:45 +0300

    created words.json, Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import style from './footer.module.scss'
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>
+            <p className={style.footerText}>2023</p>
+        </footer>
+    )
+}
