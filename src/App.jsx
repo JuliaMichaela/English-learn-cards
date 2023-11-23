4ba1d9a7704f20bec789e495448bfc82e7787b04
JuliaMichaela <juliaperez@yandex.ru>
2023-11-23 17:53:38 +0300

    created WordCardMap.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Header from './components/Header/Header'
 import './App.css'
+import Header from './components/Header/Header'
+import WordCardMap from './components/WordCardMap/WordCardMap'
 import Footer from './components/Footer/Footer'
+
 
 function App() {
 
   return (
     <>
       <Header />
+      <WordCardMap />
       <Footer />
 
     </>
---
+++ src/components/WordCardMap/WordCardMap.jsx
@@ -0,0 +1,9 @@
+import style from './WordCardMap.module.scss'
+
+export default function WordCardMap() {
+    return (
+        <div className={style.card - container}>
+
+        </div>
+    )
+}
