e1f7614930904b93714b01ece9c848b41f4a9860
JuliaMichaela <juliaperez@yandex.ru>
2023-11-23 17:53:09 +0300

    create WordCard.jsx

---
+++ src/components/WordCard/WordCard.jsx
@@ -0,0 +1,11 @@
+import style from './wordCard.module.scss'
+
+export default function WordCard(props) {
+    return (
+        <div className={style.card + style.animation}>
+            <div className={style.word}>{props.engVersion}</div>
+            <div className={style.transcription}>{props.transcription}</div>
+            <button className={style.button}>Проверить</button>
+        </div>
+    )
+}
