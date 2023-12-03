2c1f7ca17049e0834550b8e43397f0521fcbb16b
JuliaMichaela <juliaperez@yandex.ru>
2023-12-03 22:45:38 +0300

    added state

--- src/components/WordCard/WordCard.jsx
+++ src/components/WordCard/WordCard.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import style from './wordCard.module.scss'
 
 
 export default function WordCard(props) {
+
+    const [isVisible, setVisible] = useState(false);
+
+    const handleShow = () => {
+        setVisible(!isVisible)
+    }
     return (
         <div className={`${style.card} ${style.animation}`}>
             <div className={style.word}>{props.engVersion}</div>
             <div className={style.transcription}>{props.transcription}</div>
-            <button className={style.button}>Проверить</button>
+            <button className={isVisible ? "btnVisible word" : "btnTranslate"} disabled={isVisible} onClick={handleShow} >
+                {isVisible ? props.rusVersion : "Проверить"}
+            </button>
+            {/* <button className={style.button}>Проверить</button> */}
         </div>
     )
 }
