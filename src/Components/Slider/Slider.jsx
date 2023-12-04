68bead062ebdde4ffabd2d8d8f52881a658622f4
JuliaMichaela <juliaperez@yandex.ru>
2023-12-04 17:26:57 +0300

    create HomePage.jsx

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import style from '../Slider/slider.module.scss';
 
-export default function Slider({ english, transcription }) {
+export default function Slider({ english, transcription, russian }) {
+
+    const [isVisible, setVisible] = useState(false);
+
+    const handleShow = () => {
+        setVisible(!isVisible)
+    }
+
+
     return (
         <div className={`${style.card} ${style.animation}`}>
             <div className={style.word}>{english}</div>
             <div className={style.transcription}>{transcription}</div>
-            <button className={style.button}>Проверить</button>
+            <button className={isVisible ? "btnVisible word" : "btnTranslate"} disabled={isVisible} onClick={handleShow} >
+                {isVisible ? russian : "Проверить"}
+            </button>
+            {/* <button className={style.button}>Проверить</button> */}
         </div>
 
     )
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,20 @@
-
+import { Link } from 'react-router-dom'
+import style from '../HomePage/homePage.module.scss'
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <div className={style.container}>
+            <div className={style.row}>
+                <div className={style.column}>
+                    <img className={style.img} src="../../assets/images/home.jpeg" alt="learn_language" />
+                </div>
+                <div className={style.column}>
+                    <h1>Learning English Words</h1>
+                    <p>Приложение для изучения иностранного языка и расширения словарного запаса.</p>
+                    <Link to="/table">Старт</Link>
+                </div>
+            </div>
+
+        </div>
     )
 }
