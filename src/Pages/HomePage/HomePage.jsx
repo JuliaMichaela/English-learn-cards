a9663a1227d4090c25c7ddb4388a8c077d6b8b51
JuliaMichaela <juliaperez@yandex.ru>
2023-12-05 23:45:22 +0300

    added image to HomePage.jsx

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
 import style from '../HomePage/homePage.module.scss'
+import learn from '../../assets/images/home.jpeg'
 
 export default function HomePage() {
     return (
         <div className={style.container}>
             <div className={style.row}>
                 <div className={style.column}>
-                    <img className={style.img} src="../../assets/images/home.jpeg" alt="learn_language" />
+                    <img className={style.img} src={learn} alt="learn_language" />
                 </div>
                 <div className={style.column}>
                     <h1>Learning English Words</h1>
